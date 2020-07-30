d16ffbcb2498548e2d569c90dbfe146e8b040830
junji9072 <flasusmaga@gmail.com>
2020-07-30 17:25:00 +0900

    '3th HW'

--- wordcount/wordcount/urls.py
+++ wordcount/wordcount/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import wordcount_app.views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',wordcount_app.views.home,name='home'),
-    path('introduce/',wordcount_app.views.introduce, name = 'introduce'),
-    path('profile/<int:designer_id>', wordcount_app.views.detail, name = 'detail'),
+    path('',include('wordcount_app.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
