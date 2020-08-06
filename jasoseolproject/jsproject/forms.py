a984ace94e88948a03a7990cc2ddf3e32a267508
junji9072 <flasusmaga@gmail.com>
2020-08-06 20:04:48 +0900

    '실습 완료 4강 주석 정리만 하면 완료-

---
+++ jasoseolproject/jsproject/forms.py
@@ -0,0 +1,16 @@
+from django import forms # 모델폼을 쓰기 위해서 장고에서 불러온다.
+from .models import Jasosel
+
+class JssForm(forms.ModelForm):
+    class Meta:
+        model = Jasosel #어떤 모델과 대응될 지
+        fields = ('title','content',) #모델 안에서 어떤 것만 사용하겠다. updated_at은 자동적으로 생성 되므로 사용자가 입력 할 필요없어서 사용 X
+    
+    def __init__(self,*args,**kwargs): #클래스에는 첫인자는 항상 self, *args,**kwargs는 여러 인자를 받을 수 있는 파라메터
+        super().__init__(*args,**kwargs) #super는 부모클래스에서 가져와서 쓸 수 있음. 
+        self.fields['title'].label = '제목'
+        self.fields['content'].label = '자소서 내용'
+        self.fields['title'].widget.attrs.update({
+            'class' : 'jss_title',
+            'placeholder' : '제목',
+        })
--- jasoseolproject/jsproject/views.py
+++ jasoseolproject/jsproject/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import JssForm
+from .models import Jasosel
+from django.http import Http404
 
 # Create your views here.
+def index(request):
+    all_jss = Jasosel.objects.all()
+    return render(request,'index.html',{'all_jss':all_jss})
+def create(request):
+    if request.method == 'POST':
+        filled_form = JssForm(request.POST)
+        if filled_form.is_valid(): # is_valid함수를 통해 유효성 검사
+            filled_form.save()
+            return redirect('index') # index로 단순히 보내주기 때문에 redirect
+
+    jss_form = JssForm()
+    return render(request,'create.html',{'jss_form':jss_form})
+def detail(request,jss_id):
+    try:
+        my_jss = Jasosel.objects.get(pk=jss_id)
+        return render(request,'detail.html',{'my_jss':my_jss}) #각 데이터마다 만드는게 아닌 공통으로 1개만 만들어서 활용
+    except:
+        raise Http404
+def delete(request,jss_id):
+    my_jss = Jasosel.objects.get(pk=jss_id)
+    my_jss.delete()
+    return redirect('index')
+
+def update(request,jss_id):
+    my_jss = Jasosel.objects.get(pk=jss_id)
+    jss_form = JssForm(instance=my_jss)
+    if request.method == 'POST':
+        updated_form = JssForm(request.POST, instance=my_jss)
+        if updated_form.is_valid():
+            updated_form.save()
+            return redirect('index')
+    return render(request, 'create.html',{'jss_form':jss_form})
