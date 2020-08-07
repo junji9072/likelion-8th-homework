544079a60a5cebdef74a2687bd4e908ede51f8d3
junji9072 <flasusmaga@gmail.com>
2020-08-07 12:01:04 +0900

    자소설 4강 실습 정리 완료-

--- jasoseolproject/jasoseolproject/urls.py
+++ jasoseolproject/jasoseolproject/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index,name='index'),
     path('create/',create,name = 'create'),
-    path('detail/<int:jss_id>',detail,name = 'detail'),
-    path('delete/<int:jss_id>',delete,name = 'delete'),
-    path('update/<int:jss_id>',update,name = 'update'),
+    path('detail/<int:jss_id>',detail,name = 'detail'), #<int:변수명> 변수명 쓰는 이유 함수 내에 어떤 변수명으로 받을지.
+    path('delete/<int:jss_id>',delete,name = 'delete'), #url과 패스컨버터 변수를 넘겨주는 방법은 변수명 통일! 여기서 예를들어 int:hello 하면 views에서도 동일하게 변경
+    path('update/<int:jss_id>',update,name = 'update'), 
 ]
--- jasoseolproject/jsproject/views.py
+++ jasoseolproject/jsproject/views.py
@@ -17,22 +17,23 @@ def create(request):
     jss_form = JssForm()
     return render(request,'create.html',{'jss_form':jss_form})
 def detail(request,jss_id):
-    try:
-        my_jss = Jasosel.objects.get(pk=jss_id)
+    try: 
+        my_jss = Jasosel.objects.get(pk=jss_id) # 하나의 오브젝트/데이터만 전송 pk를 동적으로 변경
         return render(request,'detail.html',{'my_jss':my_jss}) #각 데이터마다 만드는게 아닌 공통으로 1개만 만들어서 활용
     except:
-        raise Http404
+        raise Http404 # 없는 오브젝트/데이터면 404에러 나오도록 한다.
+    # my_jss = get_object_or_404(Jasosel, pk=jss_id)
 def delete(request,jss_id):
-    my_jss = Jasosel.objects.get(pk=jss_id)
-    my_jss.delete()
-    return redirect('index')
+    my_jss = Jasosel.objects.get(pk=jss_id) #my_jss를 pk를 통해 잘 가져왔고, 
+    my_jss.delete() #이 함수를 통해 삭제
+    return redirect('index') #url 을 통해서도 삭제 가능!
 
 def update(request,jss_id):
     my_jss = Jasosel.objects.get(pk=jss_id)
-    jss_form = JssForm(instance=my_jss)
-    if request.method == 'POST':
-        updated_form = JssForm(request.POST, instance=my_jss)
+    jss_form = JssForm(instance=my_jss) #인스턴스를 이용해서 수정시에 이전 글이 남아있도록 함.
+    if request.method == 'POST': #수정하고 난 뒤 확인 버튼을 누르면 post방식으로 요청
+        updated_form = JssForm(request.POST, instance=my_jss) # 인스턴스를 인자로 넘겨주지 않으면, 새로운 것으로 인식
         if updated_form.is_valid():
             updated_form.save()
             return redirect('index')
-    return render(request, 'create.html',{'jss_form':jss_form})
+    return render(request, 'create.html',{'jss_form':jss_form}) #create페이지를 모델 폼을 이용해서 재활용
